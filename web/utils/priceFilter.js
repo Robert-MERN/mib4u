7dff040d58abe0c8555f6b03571f176af1fb1bcd
unknown <safeermuneeb@gmail.com>
2022-10-20 04:49:09 +0500

    first commit

---
+++ web/utils/priceFilter.js
@@ -0,0 +1,57 @@
+const queryOperator = (req, query) => {
+    let operator;
+    let minPrice = req.query.minPrice;
+    let maxPrice = req.query.maxPrice;
+    let type = req.query.type;
+    // date 2 months ago
+    var current = new Date();
+    const month = new Date(current.setMonth(current.getMonth() - 2));
+
+    if (minPrice || maxPrice) {
+        if (minPrice && !maxPrice) {
+
+            if (type === "new") {
+                operator = { $and: [query, { price: { $gte: minPrice } }, { createdAt: { $gte: month } }] }
+
+            } else if (type === "sale") {
+                operator = { $and: [query, { price: { $gte: minPrice } }, { deletedPrice: { $exists: true } }] }
+
+            } else {
+                operator = { $and: [query, { price: { $gte: minPrice } }] }
+            }
+        } else if (maxPrice && !minPrice) {
+            if (type === "new") {
+                operator = { $and: [query, { price: { $lte: maxPrice } }, { createdAt: { $gte: month } }] }
+
+            } else if (type === "sale") {
+                operator = { $and: [query, { price: { $lte: maxPrice } }, { deletedPrice: { $exists: true } }] }
+
+            } else {
+                operator = { $and: [query, { price: { $lte: maxPrice } }] }
+
+            }
+        } else if (maxPrice && minPrice) {
+            if (type === "new") {
+                operator = { $and: [query, { price: { $gte: minPrice, $lte: maxPrice } }, { createdAt: { $gte: month } }] }
+
+            } else if (type === "sale") {
+                operator = { $and: [query, { price: { $gte: minPrice, $lte: maxPrice } }, { deletedPrice: { $exists: true } }] }
+
+            } else {
+                operator = { $and: [query, { price: { $gte: minPrice, $lte: maxPrice } }] }
+            }
+        }
+    } else {
+        if (type === "new") {
+            operator = { $and: [query, { createdAt: { $gte: month } }] }
+
+        } else if (type === "sale") {
+            operator = { $and: [query, { deletedPrice: { $exists: true } }] }
+        } else {
+            operator = query
+        }
+    }
+    return operator;
+}
+
+export default queryOperator
